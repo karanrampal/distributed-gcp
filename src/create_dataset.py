c314501b9af2d997b71b7807612cdc4880c005aa
Karan Rampal <karan.rampal@hm.com>
2022-05-17 10:12:38 +0000

    Add local dataset pipeline

--- src/create_dataset.py
+++ src/create_dataset.py
@@ -1,58 +1,42 @@
 #!/usr/bin/env python3
-"""Apache beam pipeline for dataset creation"""
+"""Script for dataset creation"""
 
 import argparse
 import os
 from typing import List, Tuple
 
-from apache_beam import dataframe, Pipeline
-from apache_beam.dataframe.io import read_csv, read_parquet
-from apache_beam.options.pipeline_options import PipelineOptions
+import pandas as pd
+from sklearn.model_selection import StratifiedGroupKFold
 
 
 def arg_parser() -> Tuple[argparse.Namespace, List[str]]:
     """Parse CLI argments"""
     parser = argparse.ArgumentParser(description="Create dataset")
     parser.add_argument(
-        "-a",
-        "--annot",
-        default="gs://hm-images-bucket/annotations",
+        "-c",
+        "--castors",
+        default="gs://hm-images-bucket/annotations/castors.csv",
         type=str,
-        help="Root directory",
+        help="Castors file",
     )
     parser.add_argument(
         "-p",
         "--pim",
-        default="gs://hdl-tables/dim/dim_pim/*.parquet",
+        default="gs://hdl-tables/dim/dim_pim/",
         type=str,
-        help="Pim directory",
+        help="Directory for Pim table",
     )
     parser.add_argument(
         "-d",
         "--padma",
-        default="gs://hdl-tables/dma/product_article_datamart/*.parquet",
+        default="gs://hdl-tables/dma/product_article_datamart/",
         type=str,
-        help="Padma directory",
-    )
-    parser.add_argument(
-        "-l",
-        "--labels",
-        nargs="+",
-        default=["fitted",
-                 "regularfit",
-                 "slimfit",
-                 "oversized",
-                 "skinnyfit",
-                 "relaxedfit",
-                 "loosefit",
-                 "superskinnyfit",
-                 "musclefit"],
-        help="List of labels",
+        help="Directory for Padma table",
     )
     parser.add_argument(
         "-o",
         "--out_dir",
-        default="gs://hm-images-bucket/annotations/dataflow_output",
+        default="gs://hm-images-bucket/annotations/",
         type=str,
         help="Output directory",
     )
@@ -62,54 +46,54 @@ def arg_parser() -> Tuple[argparse.Namespace, List[str]]:
 
 def main() -> None:
     """Main function"""
-    known_args, pipeline_args = arg_parser()
+    known_args, _ = arg_parser()
 
-    with Pipeline(options=PipelineOptions(pipeline_args)) as abp:
-        pim = abp | "ReadPimTable" >> read_parquet(
-            known_args.pim, columns=["product_code", "article_code", "product_fit"]
-        )
-        padma = abp | "ReadPadmaTable" >> read_parquet(
-            known_args.padma, columns=["product_code", "article_code", "castor"]
-        )
-        castors = abp | "ReadCastorFile" >> read_csv(
-            os.path.join(known_args.annot, "castors.csv")
-        )
+    # Read input data
+    padma = pd.read_parquet(
+        known_args.padma, columns=["product_code", "article_code", "castor"]
+    )
+    pim = pd.read_parquet(
+        known_args.pim, columns=["product_code", "article_code", "product_fit"]
+    )
+    castors = pd.read_csv(known_args.castors)
 
-        # Transform padma table
-        padma = padma.drop_duplicates(keep="any")
-        padma.castor = padma.castor.astype(int)
+    # Clean data tables
+    padma = padma.drop_duplicates()
+    padma.castor = padma.castor.astype(int)
+    pim = pim.dropna(axis=0, subset=["article_code", "product_fit"])
+    pim = pim.drop_duplicates()
 
-        # Transform pim table
-        pim = pim.dropna(axis=0, subset=["article_code", "product_fit"])
-        pim = pim.drop_duplicates(keep="any")
+    # Merge pim and padma table
+    data = pim.merge(padma, on=["product_code", "article_code"], how="left")
+    data = data.drop(axis=1, labels=["product_code", "article_code"])
+    data = data[~data["product_fit"].str.contains("[", regex=False)]
 
-        # Merge pim and padma table
-        data = pim.merge(
-            padma.set_index(["product_code", "article_code"]),
-            left_on=["product_code", "article_code"],
-            right_index=True,
-            how="left",
-        )
-        data = data.drop(axis=1, labels=["product_code", "article_code"])
-        data = data[~data["product_fit"].str.contains("[", regex=False)]
+    # Merge castor data to get output
+    out = castors.merge(data, on="castor", how="inner")
+    out["labels"] = out["product_fit"].astype("category").cat.codes
 
-        # Merge castor data to get output
-        out = castors.merge(
-            data.set_index("castor"), left_on="castor", right_index=True, how="inner"
-        )
-        # Convert string labels to ints
-        out["labels"] = out["product_fit"].map(dict(zip(known_args.labels,
-                                                    range(len(known_args.labels)))))
+    # Split data into training and test dataset
+    cval = StratifiedGroupKFold(n_splits=2)
+    train_idxs, test_idxs = next(cval.split(out.path, out.labels, out.castor))
+    train_fit = out.iloc[train_idxs, :]
+    test_fit = out.iloc[test_idxs, :]
 
-        # ToDo Split data into training and test dataset
-        # cval = StratifiedGroupKFold(n_splits=2)
+    # Create automl data
+    out_gcp = out[["path", "product_fit"]].copy()
+    out_gcp["path"] = "gs://hm-images-bucket/images/" + out_gcp["path"]
+    out_gcp["mode"] = "VALIDATION"
+    out_gcp.loc[train_idxs, "mode"] = "TRAINING"
+    out_gcp = out_gcp[["mode", "path", "product_fit"]]
 
-        # Write output files
-        out.to_csv(os.path.join(known_args.out_dir, "full_fit.csv"), index=False)
-
-        # ToDo write train test split
-        # train_fit.to_csv(os.path.join(known_args.out_dir, "train.csv"), index=False)
-        # test_fit.to_csv(os.path.join(known_args.out_dir, "test.csv"), index=False)
+    # Write output files
+    out.to_csv(os.path.join(known_args.out_dir, "full_fit1.csv"), index=False)
+    train_fit.to_csv(os.path.join(known_args.out_dir, "train1.csv"), index=False)
+    test_fit.to_csv(os.path.join(known_args.out_dir, "test1.csv"), index=False)
+    out_gcp.to_csv(
+        os.path.join(known_args.out_dir, "full_fit_gcai1.csv"),
+        index=False,
+        header=False,
+    )
 
 
 if __name__ == "__main__":
