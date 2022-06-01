Fix precision error for crossentropy

src/dataloader/data_loader.py
@@ -41,7 +41,7 @@ class AttributePreddataset(Dataset):
         im_path = os.path.join(self.root, "images", im_name)
         img = Image.open(im_path).convert("RGB")
 
-        labels = torch.as_tensor(row["labels"], dtype=torch.int)
+        labels = torch.as_tensor(row["labels"], dtype=torch.int64)
 
         if self.transforms is not None:
             img = self.transforms(img)
@@ -102,7 +102,7 @@ def get_dataloader(
             shuffle = not params.distributed
         else:
             ds_dict = {
-                "file_path": "annotations/test.ccsv",
+                "file_path": "annotations/test.csv",
                 "transforms": get_transform(False, params),
             }
             shuffle = False

src/model/net.py
@@ -57,7 +57,7 @@ def avg_acc_gpu(outputs: torch.Tensor, labels: torch.Tensor) -> float:
     Returns:
         average accuracy in [0,1]
     """
-    preds = outputs.argmax(dim=1).to(torch.int32)
+    preds = outputs.argmax(dim=1).to(torch.int64)
     avg_acc = (preds == labels).to(torch.float32).mean()
     return avg_acc.item()
 